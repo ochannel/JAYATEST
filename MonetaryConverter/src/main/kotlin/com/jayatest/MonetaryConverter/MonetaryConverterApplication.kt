cb859299cbb3bc271a027054a9518cb78a9ca302
renato.soliveira81@gmail.com <renato.soliveira81@gmail.com>
2021-11-20 00:38:47 -0300

    adicionando o projeto base

---
+++ MonetaryConverter/src/main/kotlin/com/jayatest/MonetaryConverter/MonetaryConverterApplication.kt
@@ -0,0 +1,11 @@
+package com.jayatest.MonetaryConverter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MonetaryConverterApplication
+
+fun main(args: Array<String>) {
+	runApplication<MonetaryConverterApplication>(*args)
+}
---
+++ MonetaryConverter/src/test/kotlin/com/jayatest/MonetaryConverter/MonetaryConverterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jayatest.MonetaryConverter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonetaryConverterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
