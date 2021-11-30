3550249a6c22aad5dca651be26fefc28e3f5fec9
RST0521 <renatooliveira.gft@sulamerica.com.br>
2021-11-30 10:20:11 -0300

    adicionando anotação CrossOrigin para resolver problema de  acesso pagina do swagger.

--- src/main/kotlin/com/jayatest/monetaryconverter/controller/MonetaryConverterController.kt
+++ src/main/kotlin/com/jayatest/monetaryconverter/controller/MonetaryConverterController.kt
@@ -14,11 +14,12 @@ import org.springframework.web.bind.annotation.*
 @RestController
 class MonetaryConverterController(val monetaryConverterService: MonetaryConverterService) : V1Api {
 
+    @CrossOrigin
     override fun listAll() = ResponseEntity.ok().body(monetaryConverterService.listAll());
-
+    @CrossOrigin
     override fun create(monetaryConverterDTO: MonetaryConverterDTO) =
         ResponseEntity.status(HttpStatus.CREATED).body(monetaryConverterService.create(monetaryConverterDTO))
-
+    @CrossOrigin
     override fun findforidUsurious(idUsurious: Int?)
             : ResponseEntity<List<MonetaryConverterDTO>>? =
         ResponseEntity.ok().body(monetaryConverterService.findforidUsurious(idUsurious));
